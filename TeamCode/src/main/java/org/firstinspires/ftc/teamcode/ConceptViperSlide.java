fd7a1603082412d4aac25ee5d846b0731de038c5
YourAverageFTCPerson <mwang15@ocdsb.ca>
2024-10-22 16:07:35 -0400

    Add concept viper slide code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptViperSlide.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "Concept Viper Slide", group = "Concept")
+public class ConceptViperSlide extends LinearOpMode { // 4 stage 336 mm
+    private DcMotor motor; // 2 223s, 1 312
+
+    /**
+     * From goBUILDA website.
+     */
+    private static final float YELLOW_JACKET_312_PPR = 537.7f;
+
+    private static final float YELLOW_JACKET_223_PPR = 751.8f;
+
+    private static final double FULL_EXTENSION = 8.13333333333;
+
+    private static boolean USING_223 = true;
+
+    @Override
+    public void runOpMode() {
+        this.motor = this.hardwareMap.get(DcMotor.class, "viperMotor");
+        this.motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        this.motor.setPower(0d);
+        this.motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        this.motor.setTargetPosition(0); // Return to original position
+        this.motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        this.waitForStart();
+        while (this.opModeIsActive()) {
+            if (this.gamepad1.a) {
+                this.motor.setTargetPosition((int) Math.round(FULL_EXTENSION * (USING_223 ? YELLOW_JACKET_223_PPR : YELLOW_JACKET_312_PPR)));
+            } else {
+                this.motor.setTargetPosition(0);
+            }
+        }
+    }
+}
