b7770db78bd22313289a93a0ddf36887104dfdb9
Iloveducks123 <131316189+Iloveducks123@users.noreply.github.com>
2025-01-10 13:23:14 -0500

    Update CuriousAutonomous.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CuriousAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CuriousAutonomous.java
@@ -9,120 +9,134 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 public class CuriousAutonomous extends LinearOpMode {
   @Override
   public void runOpMode() throws InterruptedException {
-//
-//    long COUNTS_PER_REVOLUTION_OF_DRIVING_MOTORS = 525;
-//    double EncoderPositionsPerInch = 116.363636364;
-//    double WheelCircumferenceMM = 100 * Math.PI;
-//    long ViperCountsPerRevolution;
-//    double ViperEncoderPositionsPerInch;
-//    DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
-//    DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
-//    DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
-//    DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
-//    DcMotor viperMotor = hardwareMap.dcMotor.get("viperMotor");
-//    Servo bucketServo = hardwareMap.servo.get("bucketServo");
-//    Servo rampServo = hardwareMap.servo.get("rampServo");
-//    IMU imu = hardwareMap.get(IMU.class, "imu");
-//
-//
-//    imu.initialize(new IMU.Parameters(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingPosition.UP, RevHubOrientationOnRobot.USBFacingPosition.FORWARD)));
-//    imu.resetYaw();
-//
-//    frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//    backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//    frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//    backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//
-//    int frontLeftTarget = (int) (-96 * EncoderPositionsPerInch);
-//    int frontRightTarget = (int) (96 * EncoderPositionsPerInch);
-//    int backLeftTarget = (int) (96 * EncoderPositionsPerInch);
-//    int backRightTarget = (int) (-96 * EncoderPositionsPerInch);
-//
-//    waitForStart();
-//
-//    while (opModeIsActive()) {
-//      double yaw = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
-//
-//      frontLeftMotor.setTargetPosition(frontLeftTarget);
-//      frontRightMotor.setTargetPosition(frontRightTarget);
-//      backLeftMotor.setTargetPosition(backLeftTarget);
-//      backRightMotor.setTargetPotion(backRightTarget);
-//
-//      frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//      frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//      backLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//      backRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//
-//      while (frontLeftMotor.isBusy() && frontRightMotor.isBusy() && backLeftMotor.isBusy() && backRightMotor.isBusy()) {
-//        // wait for them to get to the position and stop moving
-//      }
-//
-//      frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//      frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//      backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//      backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//
-//      frontLeftTarget = (12 * EncoderPositionsPerInch);
-//      frontRightTarget = (12 * EncoderPositionsPerInch);
-//      backLeftTarget = (12 * EncoderPositionsPerInch);
-//      backRightTarget = (12 * EncoderPositionsPerInch);
-//
-//      frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//      frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//      backLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//      backRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//
-//      while (frontLeftMotor.isBusy() && frontRightMotor.isBusy() && backLeftMotor.isBusy() && backRightMotor.isBusy()) {
-//        // wait for them to get to the position and stop moving
-//      }
-//
-//      frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//      frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//      backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//      backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//
-//      while (Math.abs(yaw - -45) > 1) {
-//        frontRightMotor.setPower(0.5);
-//        backRightMotor.setPower(0.5);
-//        frontLeftMotor.setPower(-0.5);
-//        backLeftMotor.setPower(-0.5);
-//      }
-//      // insert viper code here
-//      bucketServo.setPosition(123);
-//
-//      bucketServo.setPosition(0);
-//
-//      //viper code again
-//
-//      while (Yaw > math.abs(0)) {
-//        frontRightMotor.setPower(-0.5);
-//        backRightMotor.setPower(-0.5);
-//        frontLeftMotor.setPower(0.5);
-//        backLeftMotor.setPower(0.5);
-//      }
-//      frontLeftTarget = (-12 * EncoderPositionsPerInch);
-//      frontRightTarget = (-12 * EncoderPositionsPerInch);
-//      backLeftTarget = -(12 * EncoderPositionsPerInch);
-//      backRightTarget = (-12 * EncoderPositionsPerInch);
-//
-//      frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//      frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//      backLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//      backRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//
-//      while (frontLeftMotor.isBusy() && frontRightMotor.isBusy() && backLeftMotor.isBusy() && backRightMotor.isBusy()) {
-//        // wait for them to get to the position and stop moving
-//      }
-//
-//      frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//      frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//      backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//      backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//
-//      int frontLeftTarget = (121 * EncoderPositionsPerInch);
-//      int frontRightTarget = (-121 * EncoderPositionsPerInch);
-//      int backLeftTarget = (-121 * EncoderPositionsPerInch);
-//      int backRightTarget = (121 * EncoderPositionsPerInch);
+  public static final float YELLOW_JACKET_312_PPR = 537.7f;
+
+  public static final float YELLOW_JACKET_223_PPR = 751.8f;
+
+  public static final double REVOLUTIONS_FOR_FULL_EXTENSION = 8.1;
+   upordown = 0; 
+   long COUNTS_PER_REVOLUTION_OF_DRIVING_MOTORS = 525;
+   double EncoderPositionsPerInch = 116.363636364;
+   double WheelCircumferenceMM = 100 * Math.PI;
+   long ViperCountsPerRevolution;
+   final int fullExtension = (int) Math.round(REVOLUTIONS_FOR_FULL_EXTENSION * (USING_223 ? YELLOW_JACKET_223_PPR : YELLOW_JACKET_312_PPR)) - 500;
+   double ViperEncoderPositionsPerInch;
+   DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+   DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+   DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+   DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+   DcMotor viperMotor = hardwareMap.dcMotor.get("viperMotor");
+   Servo bucketServo = hardwareMap.servo.get("bucketServo");
+   Servo rampServo = hardwareMap.servo.get("rampServo");
+   IMU imu = hardwareMap.get(IMU.class, "imu");
+
+
+   imu.initialize(new IMU.Parameters(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingPosition.UP, RevHubOrientationOnRobot.USBFacingPosition.FORWARD)));
+   imu.resetYaw();
+
+   frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+   backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+   frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+   backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+   int frontLeftTarget = (int) (-96 * EncoderPositionsPerInch);
+   int frontRightTarget = (int) (96 * EncoderPositionsPerInch);
+   int backLeftTarget = (int) (96 * EncoderPositionsPerInch);
+   int backRightTarget = (int) (-96 * EncoderPositionsPerInch);
+   int viperMotorTarget; b
+
+   waitForStart();
+
+   while (opModeIsActive()) {
+     double yaw = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+
+     frontLeftMotor.setTargetPosition(frontLeftTarget);
+     frontRightMotor.setTargetPosition(frontRightTarget);
+     backLeftMotor.setTargetPosition(backLeftTarget);
+     backRightMotor.setTargetPotion(backRightTarget);
+
+     frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+     frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+     backLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+     backRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+     while (frontLeftMotor.isBusy() && frontRightMotor.isBusy() && backLeftMotor.isBusy() && backRightMotor.isBusy()) {
+       // wait for them to get to the position and stop moving
+     }
+
+     frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+     frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+     backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+     backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+     frontLeftTarget = (12 * EncoderPositionsPerInch);
+     frontRightTarget = (12 * EncoderPositionsPerInch);
+     backLeftTarget = (12 * EncoderPositionsPerInch);
+     backRightTarget = (12 * EncoderPositionsPerInch);
+
+     frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+     frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+     backLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+     backRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+     while (frontLeftMotor.isBusy() && frontRightMotor.isBusy() && backLeftMotor.isBusy() && backRightMotor.isBusy()) {
+       // wait for them to get to the position and stop moving
+     }
+
+     frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+     frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+     backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+     backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+     while (Math.abs(yaw - -45) > 1) {
+       frontRightMotor.setPower(0.5);
+       backRightMotor.setPower(0.5);
+       frontLeftMotor.setPower(-0.5);
+       backLeftMotor.setPower(-0.5);
+     }
+     
+     upordown = 0;
+     viperMotor.setTargetPosition(viperTargetPosition = fullExtension);
+     while (upordown == 0) {
+        viperMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION); 
+        if (viperMotor.getCurrentPosition() - 20 > targetPosition && upordown == 0) {
+          this.motor.setPower(0.0);
+     
+     bucketServo.setPosition(123);
+
+     bucketServo.setPosition(0);
+
+     //viper code again
+
+     while (Yaw > math.abs(0)) {
+       frontRightMotor.setPower(-0.5);
+       backRightMotor.setPower(-0.5);
+       frontLeftMotor.setPower(0.5);
+       backLeftMotor.setPower(0.5);
+     }
+     frontLeftTarget = (-12 * EncoderPositionsPerInch);
+     frontRightTarget = (-12 * EncoderPositionsPerInch);
+     backLeftTarget = -(12 * EncoderPositionsPerInch);
+     backRightTarget = (-12 * EncoderPositionsPerInch);
+
+     frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+     frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+     backLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+     backRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+     while (frontLeftMotor.isBusy() && frontRightMotor.isBusy() && backLeftMotor.isBusy() && backRightMotor.isBusy()) {
+       // wait for them to get to the position and stop moving
+     }
+
+     frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+     frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+     backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+     backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+     int frontLeftTarget = (121 * EncoderPositionsPerInch);
+     int frontRightTarget = (-121 * EncoderPositionsPerInch);
+     int backLeftTarget = (-121 * EncoderPositionsPerInch);
+     int backRightTarget = (121 * EncoderPositionsPerInch);
 //// // Going for another sample
 ////       frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 ////       frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
