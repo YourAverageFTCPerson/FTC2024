8c72078bfb18480936f5f45998274329c0953a80
YourAverageFTCPerson <mwang15@ocdsb.ca>
2025-01-11 10:51:37 -0500

    idk what I'm doing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -4,6 +4,9 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
+import org.firstinspires.ftc.teamcode.util.hardware.BucketActions;
+import org.firstinspires.ftc.teamcode.util.hardware.GrabberActions;
+import org.firstinspires.ftc.teamcode.util.hardware.RampActions;
 import org.firstinspires.ftc.teamcode.util.hardware.ViperHardware;
 import org.firstinspires.ftc.teamcode.util.hardware.MecanumHardware;
 
@@ -16,6 +19,10 @@ public class Main extends LinearOpMode {
         mecanum.initialize();
 
         ViperHardware viper = new ViperHardware(this);
+
+        GrabberActions grabberActions = new GrabberActions(this, "grabber");
+        BucketActions bucketActions = new BucketActions(this);
+        RampActions rampActions = new RampActions(this);
 
         viper.initialize();
 
@@ -30,6 +37,24 @@ public class Main extends LinearOpMode {
                 viper.extend();
             } else if (gamepad1.b && !previousGamepad1.b) {
                 viper.retract();
+            }
+
+            if (gamepad1.x && !previousGamepad1.x) {
+                grabberActions.open.run(null);
+            } else if (gamepad1.y && !previousGamepad1.y) {
+                grabberActions.close.run(null);
+            }
+
+            if (gamepad1.dpad_left && !previousGamepad1.dpad_left) {
+                rampActions.lift.run(null);
+            } else if (gamepad1.dpad_right && !previousGamepad1.dpad_right) {
+                rampActions.lower.run(null);
+            }
+
+            if (gamepad1.dpad_up && !previousGamepad1.dpad_up) {
+                bucketActions.tilt.run(null);
+            } else if (gamepad1.dpad_down && !previousGamepad1.dpad_down) {
+                bucketActions.right.run(null);
             }
 
             mecanum.driveWithPower(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
